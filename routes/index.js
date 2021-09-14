a444cd6260c53da33e627f38dc761ab1fe20bcc0
eitan gayor <eitan.gayor@gmail.com>
2021-09-14 16:51:20 +0300

    committing tinymce

--- routes/index.js
+++ routes/index.js
@@ -181,7 +181,7 @@ exports.create = function (req, res, next) {
     */
 
     res.setHeader('Location', '/');
-    res.status(302).send(todo.content.toString('base64'));
+    res.status(302).send(This is a message);
 
     // res.redirect('/#' + todo.content.toString('base64'));
   });
